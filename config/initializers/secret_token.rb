bb99407f4d6b64ddc9a46184c56e575772003733
Harrison Yu <Harrison@Harrisons-MacBook-Pro.local>
2013-07-08 18:50:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mupp::Application.config.secret_token = '13297e07060b86c8494c6944f3cda9a68ece9f47c54f76e8a7a71ca88cbba7d73ceb4f469363cead7d8cca96a7c16718b06c2b64627d8b6d1f5ddd347c908424'
