d12b54b1f21f1f1ab2c1e8ca39334fe86ea3c93e
snkutkoski <snkutkoski@gmail.com>
2013-07-14 15:47:36 -0500

    song scrape

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -21,39 +21,51 @@ class SongController < ApplicationController
 
   def lastfmscrape
     
-    artist = "Daft+Punk"
-    artistUrl = "http://www.jango.com/music/#{artist}?c=1"
-    artistDoc = Nokogiri::HTML(open(artistUrl))
-    video = artistDoc.css('.artist_song_li .song_name a')[0]['href']
-    songUrl = "http://www.jango.com#{video}?c=1"
-    songDoc = Nokogiri::HTML(open(songUrl))
-    allScripts = songDoc.css('script')
-    allScripts.each do |script|
-      scriptClean = script.content.gsub(/\s+/, "")
-      puts scriptClean.scan(/"([^"]*)"/) if scriptClean.start_with? "_jango.vp.inpage"
+    artists = Artist.all
+    threads = []
+    array = []
+
+    artists.each do |artist|
+      name = artist.artistName.gsub(' ', '+')
+      artistUrl = "http://www.jango.com/music/#{name}?c=1"
+ 
+      threads << Thread.new do
+        artistDoc = Nokogiri::HTML(open(artistUrl))  
+        videos = artistDoc.css('.artist_song_li .song_name a')
+        songThreads = []
+        
+        videos.each do |video|
+          songName = video.content
+          songPath = video['href']
+          songUrl = "http://www.jango.com#{songPath}?c=1"
+          songDoc = Nokogiri::HTML(open(songUrl))
+          allScripts = songDoc.css('script')
+          songCode = ""
+          allScripts.each do |script|
+            scriptClean = script.content.gsub(/\s+/, "")
+            if scriptClean.start_with? "_jango.vp.inpage"
+              songCode = scriptClean.scan(/"([^"]*)"/)[0][0].gsub('-', '')
+            end
+          end
+          array << [songName, songCode, artist.id, "lyric"]
+        end
+        #Wait until song threads are done.
+        songThreads.each { |aThread|  aThread.join }
+      end
+    end
+    
+    #Wait until threads are done.
+    threads.each { |aThread|  aThread.join }
+
+    array.each do |x|
+      newEntry = Song.new  
+      newEntry.songName = x[0] 
+      newEntry.link = x[1]
+      newEntry.artistID = x[2] 
+      newEntry.lyrics = x[3]
+      newEntry.save
     end
     redirect_to home_path and return
-
-    # artists = Artist.all
-    # threads = []
-    # array = []
-
-    # artists.each do |artist|
-    #   name = artist.artistName.gsub(' ', '+')
-    #   artistUrl = "http://www.jango.com/music/#{name}?l=0"
-    #   threads << Thread.new do
-    #     artistDoc = Nokogiri::HTML(open(artistUrl))
-    #     videos = artistDoc.css('.artist_song_li')
-    #     videos.each do |video|
-    #       puts video
-    #   	end
-    #   end
-
-    #   # Wait until threads are done.
-    #   threads.each { |aThread|  aThread.join }
-
-    # end
-    # redirect_to home_path and return
   end
 
   def delete
