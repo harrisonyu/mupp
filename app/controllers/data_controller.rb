107ca3023373406f3833f167c5bc3cdab4ed866d
Harrison Yu <Harrison@Harrisons-MacBook-Pro.local>
2013-07-14 21:18:43 -0500

    added verification

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,4 +1,7 @@
 class DataController < ApplicationController
+
+  http_basic_authenticate_with name: "muppghs", password: "hackfest"
+
   def index
     @songs = Song.all
     @locations = Location.all
